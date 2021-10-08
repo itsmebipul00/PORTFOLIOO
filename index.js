a0c33990e21036465838d7b6cde22ab9c3193a17
bipul <itsmebipul00@gmail.com>
2021-10-08 16:25:00 +0530

    added dark-theme

---
+++ index.js
@@ -0,0 +1,31 @@
+let icon= document.querySelector('.moon-icon')
+
+
+if(localStorage.getItem('theme')==null){
+    localStorage.setItem('theme', 'light');
+    icon.src= "icons/moon.png";
+}
+
+let localDB= localStorage.getItem('theme');
+
+if(localDB=='light'){
+    icon.src= "icons/moon.png";
+    document.body.classList.remove('dark-theme');
+}else if(localDB=='dark'){
+    icon.src="icons/sun.png"
+    document.body.classList.add('dark-theme');
+}
+
+icon.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme');
+    if(document.body.classList.contains('dark-theme')){
+        icon.src ="icons/moon.png";
+        localStorage.setItem('theme', 'dark')
+        window.location.reload()
+    }else{
+        icon.src= "icons/sun.png"
+        localStorage.setItem('theme', 'light')
+        window.location.reload()
+
+    }
+})
